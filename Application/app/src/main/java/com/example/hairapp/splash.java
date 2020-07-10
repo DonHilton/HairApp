e78bf8e2cc65bb525f59620c2f5bbf24331b9718
mned447 <1840152@brunel.ac.uk>
2020-07-10 18:53:46 +0100

    fixed

---
+++ Application/app/src/main/java/com/example/hairapp/splash.java
@@ -0,0 +1,30 @@
+package com.example.hairapp;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.SparseArray;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.json.JSONObject;
+
+
+public class splash extends AppCompatActivity{
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+
+    public void openReg(View view){
+//        Intent intent = new Intent(this, subject_choice.class);
+//        startActivity(intent);
+    }
+    public void signIn(View view){
+//        Intent intent = new Intent(this, subject_choice.class);
+//        startActivity(intent);
+    }
+}
