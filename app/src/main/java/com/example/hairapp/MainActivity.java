fd42f2b79b9df34d7ed836d62b803e26ff47c499
Remia <1808329@brunel.ac.uk>
2020-07-10 20:56:44 +0100

    Customer page Navigation header finished

--- app/src/main/java/com/example/hairapp/MainActivity.java
+++ app/src/main/java/com/example/hairapp/MainActivity.java
@@ -1,14 +1,37 @@
 package com.example.hairapp;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+                setSupportActionBar(toolbar);
+                drawer = findViewById(R.id.drawer_layout);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawer,toolbar,
+                R.string.navivation_drawer_open, R.string.navivation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+    //when we press the back buttom dont want to leave immediately so
+    @Override
+    public void onBackPressed(){
+        if (drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        }else {//drawer is not open
+            super.onBackPressed();
+        }
     }
 }
