76019561d4aeb91da2bf29ea36e877bc9eb6926d
Remia <1808329@brunel.ac.uk>
2020-07-12 21:54:18 +0100

    Customer Setup page done

---
+++ app/src/main/java/com/example/hairapp/customerSetUp.java
@@ -0,0 +1,14 @@
+package com.example.hairapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class customerSetUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_set_up);
+    }
+}
---
+++ app/src/main/java/com/example/hairapp/myBookings.java
@@ -0,0 +1,14 @@
+package com.example.hairapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class myBookings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_bookings);
+    }
+}
